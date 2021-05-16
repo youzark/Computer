6bf003467015ec55c505e71b932418e58f6e83b7
hyx <gacforstudy@gmail.com>
2021-05-16 21:01:00 +0800

    parallel openmp psrs sort completed

--- parallel/openMp/src/test.sh
+++ parallel/openMp/src/test.sh
@@ -1,2 +1,15 @@
+gcc pi1.c -o test -fopenmp
+cat "pi1:"
+./test
+gcc pi2.c -o test -fopenmp
+cat "pi2:"
+./test
+gcc pi3.c -o test -fopenmp
+cat "pi3:"
+./test
+gcc pi4.c -o test -fopenmp
+cat "pi4:"
+./test
 gcc psrs.c -o test -fopenmp
+cat "psrs:"
 ./test
