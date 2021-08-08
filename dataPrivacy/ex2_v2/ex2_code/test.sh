5830596724bb9d6977648618f079318fa03af541
hyx <gacforstudy@gmail.com>
2021-08-08 23:31:46 +0800

    data privacy homework

--- dataPrivacy/ex2_v2/ex2_code/test.sh
+++ dataPrivacy/ex2_v2/ex2_code/test.sh
@@ -1,3 +1,3 @@
 # python3 main.py --mode DP --gpu=0 > DP_result
-python3 main.py --mode Paillier --num_users 2 --gpu=0 > Paillier_result
+python3 main.py --mode Paillier --num_users 2 --gpu=0
 #python3 ./paillier_test.py > Paillier_test_result
