9fa3dcf0729ae568bf7dce6e4f70bb05b44d01d9
hyx <gacforstudy@gmail.com>
2021-08-14 17:26:36 +0800

    delete

--- java/src/main/java/basic/hello_world.java
+++
@@ -1,9 +0,0 @@
-package basic;
-public class hello_world 
-{
-	public static void main(String []args) 
-	{
-		System.out.println( args[0] );
-		System.out.println( args[1] );
-	}
-}
--- java/src/test.sh
+++
@@ -1,6 +0,0 @@
-javac ./main/java/basic/hello_world.java
-
-# part 2: -cp path from cwd to dir contain package
-# basic.hello_world : package name . class name(file name)
-# hello world : args[]
-java -cp main/java basic.hello_world hello world
