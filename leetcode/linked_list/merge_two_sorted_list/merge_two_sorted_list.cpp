adf47b1f3dd229292ec3ae5a563bd4b329dd7ba7
hyx <gacforstudy@gmail.com>
2021-08-17 13:02:14 +0800

    still visited some wild pointer.

--- leetcode/linked_list/merge_two_sorted_list/merge_two_sorted_list.cpp
+++ leetcode/linked_list/merge_two_sorted_list/merge_two_sorted_list.cpp
@@ -10,8 +10,8 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) 
 	{
-		ListNode *front,*head,*temp,*trav1,*next1,*trav2,*next2;
-		int at;
+		ListNode *front,*head,*edge1,*edge2;
+
 		if(l1 == nullptr)
 		{
 			return l2;
@@ -20,60 +20,35 @@ public:
 		{
 			return l1;
 		}
-		if(l1->val < l2->val)
-		{
-			front = head = l1;
-			ListNode *vhead = new ListNode(0,l2);
-			trav1 = l1;
-			trav2 = vhead;
-			next1 = trav1->next;
-			next2 = trav2->next;
-			at = 1;
-		}
-		else
-		{
-			front = head = l2;
-			ListNode *vhead = new ListNode(0,l1);
-			trav1 = vhead;
-			trav2 = l2;
-			next1 = trav1->next;
-			next2 = trav2->next;
-			at = 2;
-		}
+		//guarantee l1 and l2 are not empty
 
-		while(next1 != nullptr && next2 != nullptr)
+		// front is the last node we add to the ans.linked_list
+		// edge is the two node(or null) waiting to be appended to ans
+
+		// init front and edge
+		edge1 = l1;
+		edge2 = l2;
+		front = edge1->val < edge2->val ? edge1 : edge2;
+		head = front;
+		edge1 = edge1->val < edge2->val ? edge1->next : edge1;
+		edge2 = edge1->val < edge2->val ? edge2 : edge2->next;
+
+		while(edge1 != nullptr && edge2 != nullptr)
 		{
-			if(next1->val < next2->val)
+			if(edge1->val < edge2->val)
 			{
-				trav1 = next1;
-				next1 = next1->next;
-				if(at == 2)
-				{
-					front->next = trav1;
-					front = front->next;
-					at = 1;
-				}
+				front->next = edge1;
+				edge1 = edge1->next;
+				front = front->next;
 			}
 			else
 			{
-				trav2 = next2;
-				next2 = next2->next;
-				if(at == 1)
-				{
-					front->next = trav2;
-					front = front->next;
-					at = 1;
-				}
+				front->next = edge2;
+				edge2 = edge2->next;
+				front = front->next;
 			}
 		}
-		if(next1 == nullptr)
-		{
-			trav1->next = trav2;
-		}
-		else
-		{
-			trav2->next = trav1;
-		}
+		front->next = (edge1 == nullptr) ? edge2 : edge1;
 		return head;
     }
 };
